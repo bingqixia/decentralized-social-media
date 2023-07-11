5c079116997abc47878bba2b88d10ba232767309
Pookii <bingqi.xia@gmail.com>
2023-07-11 17:30:02 +0100

    add: contract for project

---
+++ project/hardhat/hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: process.env.GOERLI_INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+}
